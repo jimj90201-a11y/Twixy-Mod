aefbb2f7ab1a02a5389fb55056d3cb3de9ab3834
jimj90201-a11y <jimj90201@gmail.com>
2026-06-20 17:22:38 +0530

    Create ModEntities class for entity registration
    This class defines the ModEntities for the Twixy mod, including the registration of the TWIXY entity type.

---
+++ src/main/java/com/twixy/mod/entity/ModEntities.java
@@ -0,0 +1,21 @@
+package com.twixy.mod.entity;
+
+import com.twixy.mod.TwixyMod;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
+
+public class ModEntities {
+
+    public static final EntityType<TwixyEntity> TWIXY = Registry.register(
+        Registries.ENTITY_TYPE,
+        new Identifier("twixy-mod", "twixy"),
+        EntityType.Builder.create(TwixyEntity::new, SpawnGroup.CREATURE)
+            .dimensions(0.6f, 0.7f)
+            .build()
+    );
+
+    public static void register() {}
+}
