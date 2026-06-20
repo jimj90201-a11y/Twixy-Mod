dd88c9ea8ece0b6c6a04771de4fbd2505a065370
jimj90201-a11y <jimj90201@gmail.com>
2026-06-20 16:55:24 +0530

    Add files via upload

---
+++ src/client/java/com/twixy/mod/client/TwixyModClient.java
@@ -0,0 +1,10 @@
+package com.twixy.mod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TwixyModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/twixy/mod/client/TwixyModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.twixy.mod.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TwixyModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
