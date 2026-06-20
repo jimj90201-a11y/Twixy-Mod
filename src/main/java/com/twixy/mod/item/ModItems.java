7bf7c7e5676c9beb8de3387a65f83fe23d2518d7
jimj90201-a11y <jimj90201@gmail.com>
2026-06-20 17:25:20 +0530

    Create ModItems.java

---
+++ src/main/java/com/twixy/mod/item/ModItems.java
@@ -0,0 +1,19 @@
+package com.twixy.mod.item;
+
+import com.twixy.mod.entity.ModEntities;
+import net.minecraft.item.Item;
+import net.minecraft.item.SpawnEggItem;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item TWIXY_SPAWN_EGG = Registry.register(
+        Registries.ITEM,
+        new Identifier("twixy-mod", "twixy_spawn_egg"),
+        new SpawnEggItem(ModEntities.TWIXY, 0xaaaaaa, 0x555555, new Item.Settings())
+    );
+
+    public static void register() {}
+}
