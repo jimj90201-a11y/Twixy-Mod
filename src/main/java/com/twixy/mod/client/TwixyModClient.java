6cf1afb823346fb1755838ef098b158e9546e994
jimj90201-a11y <jimj90201@gmail.com>
2026-06-25 10:37:19 +0530

    Remove Twixy entity renderer registration
    Removed registration of the Twixy entity renderer.

--- src/main/java/com/twixy/mod/client/TwixyModClient.java
+++ src/main/java/com/twixy/mod/client/TwixyModClient.java
@@ -1,16 +1,9 @@
 package com.twixy.mod.client;
 
-import com.twixy.mod.entity.ModEntities;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.client.render.entity.CatEntityRenderer;
 
 public class TwixyModClient implements ClientModInitializer {
-
     @Override
     public void onInitializeClient() {
-
-        EntityRendererRegistry.register(ModEntities.TWIXY, CatEntityRenderer::new);
-
     }
 }
