23d9050a487894b74bdbb78b6b3b5cf7f7cf3e18
jimj90201-a11y <jimj90201@gmail.com>
2026-06-25 10:39:19 +0530

    Clean up onInitializeClient method
    Removed comment from onInitializeClient method.

--- src/client/java/com/twixy/mod/client/TwixyModClient.java
+++ src/client/java/com/twixy/mod/client/TwixyModClient.java
@@ -3,8 +3,7 @@ package com.twixy.mod.client;
 import net.fabricmc.api.ClientModInitializer;
 
 public class TwixyModClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+    @Override
+    public void onInitializeClient() {
+    }
 }
