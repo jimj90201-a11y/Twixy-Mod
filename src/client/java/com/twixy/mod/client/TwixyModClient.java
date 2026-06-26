1b29ec620de514e969215168f1c095771825d850
jimj90201-a11y <jimj90201@gmail.com>
2026-06-26 17:02:48 +0530

    Update TwixyModClient.java

--- src/client/java/com/twixy/mod/client/TwixyModClient.java
+++ src/client/java/com/twixy/mod/client/TwixyModClient.java
@@ -5,5 +5,10 @@ import net.fabricmc.api.ClientModInitializer;
 public class TwixyModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        EntityRendererRegistry.register(ModEntities.TWIXY, TwixyRenderer::new);
+EntityModelLayerRegistry.registerModelLayer(
+    TwixyModel.LAYER_LOCATION,
+    TwixyModel::getTexturedModelData
+);
     }
 }
