21d9d093d6ffa21c1d6d5e419b04286303b2866e
jimj90201-a11y <jimj90201@gmail.com>
2026-06-26 16:42:29 +0530

    Add attribute registration for TWIXY entity
    Register default attributes for the TWIXY entity.

--- src/main/java/com/twixy/mod/TwixyMod.java
+++ src/main/java/com/twixy/mod/TwixyMod.java
@@ -1,9 +1,13 @@
 package com.twixy.mod;
 
 import net.fabricmc.api.ModInitializer;
+
 import com.twixy.mod.entity.ModEntities;
 import com.twixy.mod.item.ModItems;
 import com.twixy.mod.sound.ModSounds;
+
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.minecraft.entity.passive.CatEntity;
 
 public class TwixyMod implements ModInitializer {
 
@@ -13,6 +17,11 @@ public class TwixyMod implements ModInitializer {
         ModEntities.register();
         ModItems.register();
         ModSounds.register();
+
+        FabricDefaultAttributeRegistry.register(
+                ModEntities.TWIXY,
+                CatEntity.createCatAttributes()
+        );
 
         System.out.println("Twixy Mod Loaded!");
     }
