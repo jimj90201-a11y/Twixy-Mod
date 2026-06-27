004a6ea3f641bcb3dbeb01a63660adf917fa5803
jimj90201-a11y <jimj90201@gmail.com>
2026-06-27 17:56:03 +0530

    Add entity rendering and model registration

--- src/client/java/com/twixy/mod/client/TwixyModClient.java
+++ src/client/java/com/twixy/mod/client/TwixyModClient.java
@@ -2,6 +2,13 @@ package com.twixy.mod.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+
+import com.twixy.mod.entity.ModEntities;
+import com.twixy.mod.client.model.TwixyModel;
+import com.twixy.mod.client.renderer.TwixyRenderer;
+
 public class TwixyModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
