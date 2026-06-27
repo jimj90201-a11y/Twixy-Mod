34d2fd21e9afda8c7e2a16138c14d7504e3e66ad
jimj90201-a11y <jimj90201@gmail.com>
2026-06-27 17:59:40 +0530

    Add import for TwixyEntity in TwixyModel

--- src/client/java/com/twixy/mod/client/model/TwixyModel.java
+++ src/client/java/com/twixy/mod/client/model/TwixyModel.java
@@ -9,6 +9,7 @@ import net.minecraft.client.model.*;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
+import com.twixy.mod.entity.TwixyEntity;
 
 public class TwixyModel extends EntityModel<TwixyEntity> {
 	private final ModelPart body;
