b25aed1dec30a31b4a12cb33e2635d729cc0717b
jimj90201-a11y <jimj90201@gmail.com>
2026-06-27 17:49:34 +0530

    Clean up imports in TwixyEntity.java
    Removed unused imports related to client rendering and models.

--- src/main/java/com/twixy/mod/entity/TwixyEntity.java
+++ src/main/java/com/twixy/mod/entity/TwixyEntity.java
@@ -5,13 +5,6 @@ import net.minecraft.entity.passive.CatEntity;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.world.World;
 import com.twixy.mod.sound.ModSounds;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
-
-import com.twixy.mod.entity.ModEntities;
-import com.twixy.mod.client.renderer.TwixyRenderer;
-import com.twixy.mod.client.model.TwixyModel;
 
 public class TwixyEntity extends CatEntity {
 
